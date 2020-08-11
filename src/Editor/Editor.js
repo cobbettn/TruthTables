8a1736165af574fb4d2b2f70a19a570e3d22db00
Joy LeViere <joyleviere@JoyLeVieresMBP.fios-router.home>
2020-08-11 18:55:17 -0400

    Added Clear button but not yet functional

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -7,6 +7,7 @@ import OperatorPicker from './OperatorPicker/OperatorPicker';
 import SchemaBuilder from './SchemaBuilder/SchemaBuilder';
 import Context from '../context';
 import dragEnd from './dragEnd';
+import { Button } from '@material-ui/core';
 
 const Editor = () => {
   const { schema, setSchema } = useContext(Context);
@@ -26,6 +27,14 @@ const Editor = () => {
       </Box>
       <Box mt="1rem">
         <SchemaBuilder/>
+      </Box>
+      <Box pt="1rem" style={{display: 'flex', flexDirection: 'row-reverse'}}>
+        <Button 
+          variant="outlined" 
+          color="secondary"
+        >
+          Clear
+        </Button>
       </Box>
     </DragDropContext>
   );
