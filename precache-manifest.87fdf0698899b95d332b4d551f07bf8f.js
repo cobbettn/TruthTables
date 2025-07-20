8aefcfa00b34e6a47495c032022c667e7140ed9d
Nathan Cobbett <cobbett.n@gmail.com>
2025-07-20 08:53:54 -0400

    homepage field test

---
+++ precache-manifest.87fdf0698899b95d332b4d551f07bf8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d781b20fb1a4af3ff2303108184d04",
+    "url": "/TruthTables/index.html"
+  },
+  {
+    "revision": "c22dad550d97035ec323",
+    "url": "/TruthTables/static/css/main.2fe8e60e.chunk.css"
+  },
+  {
+    "revision": "9cfa09ed9df24820847f",
+    "url": "/TruthTables/static/js/2.eda913f3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/TruthTables/static/js/2.eda913f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22dad550d97035ec323",
+    "url": "/TruthTables/static/js/main.6b7a64e6.chunk.js"
+  },
+  {
+    "revision": "ecfbecba141a5e9f6226",
+    "url": "/TruthTables/static/js/runtime-main.d7145f3f.js"
+  },
+  {
+    "revision": "84fd1fc9861a05a0bdaa28c01ab49496",
+    "url": "/TruthTables/static/media/GitHub_Logo_White.84fd1fc9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.337f3adfca2cf493256cfa35f5d65d4f.js"
+  "/TruthTables/precache-manifest.87fdf0698899b95d332b4d551f07bf8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TruthTables/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
